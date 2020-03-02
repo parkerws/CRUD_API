b380e7124fdb390307accd9aec5dd0902eca49b6
parkerws <parkerws3824@gmail.com>
2020-03-02 14:28:11 -0500

    Created API with CRUD operations

---
+++ CRUD_Operation/Repository/IPostRepository.cs
@@ -0,0 +1,19 @@
+﻿using CRUD_Operation.Models;
+using CRUD_Operation.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Operation.Repository
+{
+    public interface IPostRepository
+    {
+        Task<List<Category>> GetCategories();
+        Task<List<PostViewModel>> GetPosts();
+        Task<PostViewModel> GetPost(int? postId);
+        Task<int> AddPost(Post post);
+        Task<int> DeletePost(int? postId);
+        Task UpdatePost(Post post);
+    }
+}
---
+++ CRUD_Operation/Startup.cs
@@ -0,0 +1,57 @@
+using CRUD_Operation.Models;
+using CRUD_Operation.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace CRUD_Operation
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<BlogDBContext>(options =>
+            options.UseSqlServer(Configuration["ConnectionStrings:BlogDBConnection"]));
+
+            services.AddTransient<IPostRepository, PostRepository>();
+
+            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddControllers();
+
+            services.AddCors(option => option.AddPolicy("MyBlogPolicy", builder => {
+                builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+            }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
